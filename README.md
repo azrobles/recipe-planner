314af5c66397b7d8230ab9a37cd5426db59b5f28
Azucena Robles <kalea_yue@yahoo.es>
2021-03-27 04:25:06 -0700

    Spring project initial structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # recipe-planner
 A simple recipe planner
+
+Version 0.0.1-SNAPSHOT: 27 mar 2020
+
+Copyright © 2020 Azucena Robles Alonso
---
+++ src/test/java/com/ara/recipeplanner/RecipePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ara.recipeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
