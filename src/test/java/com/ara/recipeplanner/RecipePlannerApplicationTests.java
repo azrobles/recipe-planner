2c5e41a782bfea075b65ef84829268285727a451
Azucena Robles <kalea_yue@yahoo.es>
2021-04-08 10:09:00 -0700

    Context loads integration test

--- src/test/java/com/ara/recipeplanner/RecipePlannerApplicationTests.java
+++ src/test/java/com/ara/recipeplanner/RecipePlannerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ara.recipeplanner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ara.recipeplanner.controller.SupermarketController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecipePlannerApplicationTests {
 
+	@Autowired
+	private SupermarketController supermarketController;
+
 	@Test
 	void contextLoads() {
+		assertThat(supermarketController).isNotNull();
 	}
 
 }
