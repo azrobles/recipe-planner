1bfa090f7873926172eb3d5471d2cc1adc790234
Azucena Robles <kalea_yue@yahoo.es>
2021-04-21 10:50:51 -0700

    Adding a 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { SupermarketListComponent } from './supermarket-list/supermarket-list.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'supermarkets', component: SupermarketListComponent }
+  { path: 'supermarkets', component: SupermarketListComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { SupermarketListComponent } from './supermarket-list/supermarket-list.component';
@@ -13,6 +14,7 @@ import { SupermarketListComponent } from './supermarket-list/supermarket-list.co
   declarations: [
     AppComponent,
     NavbarComponent,
+    PageNotFoundComponent,
     HomeComponent,
     AdminComponent,
     SupermarketListComponent
