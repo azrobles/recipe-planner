e678f7e385bba54a98be9530249aa5c33b116283
Azucena Robles <kalea_yue@yahoo.es>
2021-04-23 05:31:31 -0700

    SupermarketListComponent getSupermarkets

--- src/app/supermarket-list/supermarket-list.component.spec.ts
+++ src/app/supermarket-list/supermarket-list.component.spec.ts
@@ -1,14 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { Supermarket } from '../models/supermarket.model';
+import { SupermarketService } from '../services/supermarket.service';
 
 import { SupermarketListComponent } from './supermarket-list.component';
 
 describe('SupermarketListComponent', () => {
   let component: SupermarketListComponent;
   let fixture: ComponentFixture<SupermarketListComponent>;
+  let getSupermarketsSpy: jasmine.Spy;
+  let testSupermarkets: Supermarket[];
+
+  const errorMessage = () => {
+    const el = fixture.nativeElement.querySelector('[type="danger"]');
+    return el ? el.textContent : null;
+  };
 
   beforeEach(async () => {
+    testSupermarkets = [{id: 1, name: 'Test Data'}];
+
+    const supermarketService = jasmine.createSpyObj('SupermarketService',
+      ['getSupermarkets']);
+    getSupermarketsSpy = supermarketService.getSupermarkets.and
+      .returnValue(of(testSupermarkets));
+
     await TestBed.configureTestingModule({
-      declarations: [ SupermarketListComponent ]
+      declarations: [ SupermarketListComponent ],
+      providers: [{provide: SupermarketService, useValue: supermarketService}]
     })
     .compileComponents();
   });
@@ -16,10 +34,42 @@ describe('SupermarketListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SupermarketListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should not show supermarkets before OnInit', () => {
+    expect(fixture.nativeElement.querySelector('table>tbody>tr'))
+      .toBeFalsy('nothing displayed');
+    expect(errorMessage()).toBeNull('should not show error element');
+    expect(getSupermarketsSpy.calls.any())
+      .toBe(false, 'getSupermarkets not yet called');
+  });
+
+  it('should show supermarkets after component initialized', () => {
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('table>tbody>tr>th:nth-of-type(1)').textContent)
+      .toBe(testSupermarkets[0].id + '');
+    expect(fixture.nativeElement.querySelector('table>tbody>tr>td:nth-of-type(1)').textContent)
+      .toBe(testSupermarkets[0].name);
+    expect(getSupermarketsSpy.calls.any()).toBe(true, 'getSupermarkets called');
+  });
+
+  it('should display error when SupermarketService fails', fakeAsync(() => {
+    getSupermarketsSpy.and
+      .returnValue(throwError('SupermarketService test failure'));
+
+    fixture.detectChanges();
+
+    tick();
+
+    fixture.detectChanges();
+
+    expect(errorMessage()).toMatch(/test failure/, 'should display error');
+    expect(fixture.nativeElement.querySelector('table>tbody>tr'))
+      .toBeFalsy('nothing displayed');
+  }));
 });
--- src/app/supermarket-list/supermarket-list.component.ts
+++ src/app/supermarket-list/supermarket-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, startWith } from 'rxjs/operators';
+import { Supermarket } from '../models/supermarket.model';
+import { SupermarketService } from '../services/supermarket.service';
 
 @Component({
   selector: 'app-supermarket-list',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupermarketListComponent implements OnInit {
 
-  constructor() { }
+  errorMessage!: string;
+  supermarkets$!: Observable<Supermarket[]>;
+
+  constructor(
+    private supermarketService: SupermarketService
+  ) { }
 
   ngOnInit(): void {
+    this.getSupermarkets();
+  }
+
+  getSupermarkets(): void {
+    this.errorMessage = '';
+    this.supermarkets$ = this.supermarketService.getSupermarkets().pipe(
+      startWith([]),
+      catchError( (err: any) => {
+        setTimeout(() => this.errorMessage = err.message || err.toString());
+        return of([]);
+      })
+    );
   }
 
 }
